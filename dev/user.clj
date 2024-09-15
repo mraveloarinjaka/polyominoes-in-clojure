337b134efe39b3ecae9cc6b47f570356b248e7d7
Mandimby RAVELOARINJAKA <mandimby.raveloarinjaka@gmail.com>
2024-09-15 14:45:22 -0400

    [FIX] (dev): allow running unit tests and benchmarks as dev

--- dev/user.clj
+++ dev/user.clj
@@ -2,6 +2,8 @@
   (:require [borkdude.deflet :refer [deflet]]
             [clojure.repl.deps :as deps]
             [clojure.test :as xunit]
+            [polyominoes.core]
+            [polyominoes.core-test]
             [taoensso.timbre :as log]))
 
 (set! *warn-on-reflection* true)
@@ -10,20 +12,23 @@
 
 (println "user.clj loaded...")
 
+#_(polyominoes.core/-main "6" "--generator" "tesser")
+
 (comment
 
   (deps/add-lib 'criterium/criterium)
   (require '[criterium.core :as crt])
-  (require '[polyominoes.core])
 
   (let [cells 5]
     (doseq [generator [:default :reducer :tesser :transducer]]
       (log/set-min-level! :info)
       (log/info "generator" generator)
-      (crt/quick-bench (polyominoes.core/nbOfPolyominoes {:cells cells :generator  generator}))))
+      (crt/quick-bench (polyominoes.core/nbOfPolyominoes {:cells cells
+                                                          :generator generator}))))
 
   (crt/with-progress-reporting
-    (crt/quick-bench (polyominoes.core/nbOfPolyominoes {:cells 5})))
+    (crt/quick-bench (polyominoes.core/nbOfPolyominoes {:cells 9
+                                                        :generator :tesser})))
 
   (comment))
 
